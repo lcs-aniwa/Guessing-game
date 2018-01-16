63539d65daea8e4bc5d97ad559502c4cc62578e2
Niwa Ayana <aniwa@lcs-mba3092.lcs.on.ca>
2018-01-16 13:57:24 -0500

    connected view to controller

--- Guessing game/ViewController.swift
+++ Guessing game/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: Outletts
+    @IBOutlet weak var numberGuess: UITextField!
+    // MARK: Properties (variables)
+    // MARK: Overrides
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +23,27 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: Action
+    @IBAction func guessSubmitted(_ sender: Any) {
+        //Pring the number that was guessed
+        print(numberGuess.text)
+        // Use a guard statement to bind to a non-optiocal variable
+        guard let inputGiven = numberGuess.text else {
+            // if the input was nil, stio and return (exit) the function return
+            return
+        }
+        // Use a guard statement to attempt to create an integer
+        guard let integerGiven = Int(inputGiven) else {
+            //if the input was test (e.g.: "five") we can't make an integeer, so stop and return (exit)the function
+            return
+        }
+        // Print the number that was guessed again
+        print(integerGiven)
+
+    }
+    
+    // MARK: Custom function(s)
 
 
 }
